a322ec35b96e2e2d13cadffb4308b4b7c1604810
P. Cassiman <fenixonecomputers@gmail.com>
2019-05-02 20:14:25 +0200

    Updated formatting to follow PEP8

--- src/python/main.py
+++ src/python/main.py
@@ -3,6 +3,8 @@ Stable version of the shape and colour detection.
 This script uses a webcam connected to the computer and detects the three primary colours.
 """
 
+# TODO: Use a class to avoid having to use the global statement
+
 ############################################# Imports ##############################################
 
 import time
@@ -10,21 +12,21 @@ import math
 
 import cv2
 import imutils
-import numpy as np
-
-from message import print_message
+# import numpy as np
 
 ######################################### Global variables #########################################
 
 __author__ = 'P. Cassiman'
 __version__ = '1.5.0'
 
+# Vision variables are placed here for easy tuning.
+
 THRESHOLD_VALUE = 90
 EDGE_LOWER_VALUE = 25
 EDGE_UPPER_VALUE = 145
 
 PERIMETER_LOWER_BOUND = 80
-PERIMETER_UPPER_BOUND = 800
+PERIMETER_UPPER_BOUND = 400
 
 MAIN_FRAME_RESOLUTION = (960, 720)
 SECONDARY_FRAME_RESOLUTION = (720, 540)
@@ -41,22 +43,28 @@ FL_X2 = 1150
 FL_Y1 = 70
 FL_Y2 = 280
 
-finish_line_1 = (FL_X1, FL_Y1)
-finish_line_2 = (FL_X2, FL_Y2)
+FINISH_LINE_1 = (FL_X1, FL_Y1)
+FINISH_LINE_2 = (FL_X2, FL_Y2)
 
-Finish_colour = (0, 0, 255)
+FINISH_LINE_COLOUR = (0, 0, 255)
 FINISHED = False
 
-lap_times = []
-start_time = time.time()
-lap = 0
+LAP_TIMES = []
+START_TIME = time.time()
+CURRENT_LAP = 0
 
 ############################################# Classes ##############################################
+
+class Finish(object):
+    """
+    * To implement
+    """
+    # TODO: implement finish line class, to avoid using global variables.
 
 ############################################ Functions #############################################
 
 
-def name_colour(pixel_colour):
+def name_colour(pixel_colour: tuple = (0, 0, 0)) -> str:
     """
     Function to find and name to colour of a given pixel.
     The colour is given in a tuple in BGR format.
@@ -65,17 +73,25 @@ def name_colour(pixel_colour):
         pixel_colour {tuple} -- The colour in BGR format.
 
     Returns:
-        int -- The name of the colour.
+        str -- The name of the colour.
     """
-
+    # Split the pixel into three separate variables.
     blue = pixel_colour[0]
     green = pixel_colour[1]
     red = pixel_colour[2]
+
+    # Start with a blank name.
     c_name = ''
+
+    # If all colours are above 250 the colour is white.
     if blue > 250 and green > 250 and red > 250:
         c_name = 'white'
+    # If all colours are below 50 the colour is black.
     elif blue < 50 and green < 50 and red < 50:
         c_name = 'black'
+
+    # Extract the other primary colours.
+    # TODO: improve the "algoritm" for checking colours.
     elif blue > green and blue > red:
         c_name = 'blue'
     elif green > blue and green > red:
@@ -87,74 +103,122 @@ def name_colour(pixel_colour):
 
 
 def finished():
-    global lap_times
-    global start_time
-    global lap
+    """
+    Function triggered when the car crosses the finish line.
+
+    """
+    # !Should not be here
+    # Access the global variables.
+    global LAP_TIMES
+    global START_TIME
+    global CURRENT_LAP
+    # Set the maximum amount of laps.
     max_laps = 3
-    lap += 1
-    if (lap <= max_laps+1):
-        if lap == 1:
-            lap_times = []
-            start_time = time.time()
+    # Increment the current lap.
+    CURRENT_LAP += 1
+    # If the current lap is less then the maximum amount of allowed laps.
+    if CURRENT_LAP <= max_laps + 1:
+        # If it's the first lap, start the timer.
+        if CURRENT_LAP == 1:
+            LAP_TIMES = []
+            START_TIME = time.time()
             print()
             print("Starting times")
         else:
-            lap_time = time.time() - start_time
+            # During the other laps, calculate the lap time.
+            lap_time = time.time() - START_TIME
+            # If the lap time is longer than 5 seconds we can proceed.
+            # This is done to filter out glitches in the vision system.
             if lap_time >= 5:
-                start_time = time.time()
-
-                # print("Crossed finish line!")
-                print("lap: {}, lap time: {}".format(lap-1, lap_time))
-                lap_times.append(lap_time)
+                # Set a new start time.
+                START_TIME = time.time()
+                # print the time to the terminal
+                # TODO: add functionality to add lap time to the screen.
+                print("lap: {}, lap time: {}".format(CURRENT_LAP - 1, lap_time))
+                # Add the lap time to the list of lap times for later reference.
+                LAP_TIMES.append(lap_time)
             else:
-                lap -= 1
-    if lap == max_laps+1:
+                # If the lap isn't longer than five seconds, undo the lap increment.
+                CURRENT_LAP -= 1
+    # If the current lap is the maximum (or last) lap.
+    if CURRENT_LAP == max_laps + 1:
+        # Print finished to the terminal.
         print('Finished')
-        average = sum(lap_times) / len(lap_times)
-        fastest = min(lap_times)
+        # Calculate the average and fastest lap times.
+        average = sum(LAP_TIMES) / len(LAP_TIMES)
+        fastest = min(LAP_TIMES)
+        # Print the results to the terminal.
         print("Average time: {}, record:{}".format(average, fastest))
         print()
-        lap = 0
+        # Reset the current lap
+        CURRENT_LAP = 0
 
 
 def print_message():
-    # Variables for the message
+    """
+    Will write a simple message to the terminal when the program first starts.
+    To show the name and author of the program.
+    """
+    # Variables for the message.
     message_width = 120
     message_1 = "Vision system for linefollowers"
     message_2 = "Based on OpenCV"
     message_3 = "Written by P. Cassiman"
 
-    # Clearing the screen
+    # Clearing the screen.
     print()
     print()
-    # Starting with the border around the message
+    # Starting with the border around the message.
     print("#" * message_width)
     print("#" + " "*(message_width - 2) + "#")
 
+    # Call the function to correctly format the message lines.
     print(create_line(message_width, message_1))
     print(create_line(message_width, message_2))
     print(create_line(message_width, ""))
     print(create_line(message_width, message_3))
 
-    # Ending with the border around the screen
+    # Ending with the border around the screen.
     print("#" + " "*(message_width - 2) + "#")
     print("#" * message_width)
-    # Adding more whitespace at the end
+    # Adding more whitespace at the end.
     print()
     print()
 
 
-def create_line(width, message):
+def create_line(width: int = 80, message: str = "Hello world!") -> str:
+    """
+    Will format a string to the given width.
+    The line will start and end with a "#".
+    The message is centered, the rest of the space is filled with whitespace.
+
+    Keyword Arguments:
+        width {int} -- The target width to format. (default: {80})
+        message {str} -- The message to be formatted. (default: {"Hello world!"})
+
+    Returns:
+        str -- The formatted line as a string.
+    """
+    # Start with a blank line.
     line = ""
+    # Add the first "#" sign.
     line += "#"
-    space = (width-2-len(message)) / 2
+    # Calculate the needed whitespace.
+    space = (width - 2 - len(message)) / 2
+    # The left white space is floored and converted to an int.
     space_left = int(math.floor(space))
+    # The right white spae is ceiled and converted to an int.
     space_right = int(math.ceil(space))
 
+    # Add the left white space.
     line += " " * space_left
+    # Add the actual message.
     line += message
+    # Add the right whitespace.
     line += " " * space_right
+    # Add the closing "#" sign.
     line += "#"
+    # Return the line.
     return line
 
 ############################################### Main ###############################################
@@ -245,17 +309,17 @@ if __name__ == "__main__":
 
         # Detect the edges in the image.
         EDGED = cv2.Canny(THRESHOLD, 25, 145)
-        # Reduce noise on the detected edges
+        # Reduce noise on the detected edges.
         # EDGED = cv2.GaussianBlur(EDGED, (3, 3), 0)
 
         if SAVE_FRAMES:
-            # Create a colour copy of the edge frame
+            # Create a colour copy of the edge frame.
             EDGED_COPY = cv2.cvtColor(EDGED, cv2.COLOR_GRAY2BGR)
 
-        # Find the contours in the image
+        # Find the contours in the image.
         CONTOURS = cv2.findContours(
             EDGED.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # Get the contours (to deal with different versions of OpenCV)
+        # Get the contours (to deal with different versions of OpenCV).
         CNTS = imutils.grab_contours(CONTOURS)
 
         for c in CNTS:
@@ -263,56 +327,58 @@ if __name__ == "__main__":
             PERIMETER = cv2.arcLength(c, True)
 
             if PERIMETER_LOWER_BOUND <= PERIMETER <= PERIMETER_UPPER_BOUND:
-                # Approximate the shape based on the perimeter (with a margin; here 3%)
+                # Approximate the shape based on the perimeter (with a margin; here 3%).
                 approx = cv2.approxPolyDP(c, 0.03 * PERIMETER, True)
                 # Draw the contour.
                 if len(approx) == 3:
                     # Triangles.
                     M = cv2.moments(c)
                     if M['m00'] != 0:
-                        # Extract the centre of the triangle coordinates
+                        # Extract the centre of the triangle coordinates.
                         cX = int(M['m10'] / M['m00'])
                         cY = int(M['m01'] / M['m00'])
-                        # Name the colour of the triangle
+                        # Name the colour of the triangle.
                         colour = FRAME[cY, cX]
                         colour_name = name_colour(colour)
-                        # If the triangle is red
+                        # If the triangle is red.
                         if colour_name == 'red':
-                            # Create a bounding rectangle for the triangle
+                            # Create a bounding rectangle for the triangle.
                             (x, y, w, h) = cv2.boundingRect(approx)
-                            # Calculate the aspect ratio of the bounding box
+                            # Calculate the aspect ratio of the bounding box.
                             ar = w / float(h)
-                            # Our triangles are half a square (diagonally) use this to further filter
-                            # out noise.
+                            # Our triangles are half a square (diagonally) use this to further
+                            # filter out noise.
                             if 0.45 <= ar <= 2.05:
-                                # Draw the contour on the frame
+                                # Draw the contour on the frame.
                                 cv2.drawContours(FRAME, [c], -1, (0, 0, 255), 2)
-                                # Write text on the resulting frame
+                                # Write text on the resulting frame.
                                 cv2.putText(FRAME, 'Car', (cX-16, cY-16), cv2.FONT_HERSHEY_SIMPLEX,
                                             1, TXT_COLOUR, 2)
                                 # TODO: Implement lap times
                                 cv2.putText(FRAME, 'Lap time', (cX - 16, cY + 16),
                                             cv2.FONT_HERSHEY_SIMPLEX, 1, TXT_COLOUR, 2)
 
-                                # Draw the bounding box on the edged frame
+                                # Draw the bounding box on the edged frame.
                                 cv2.rectangle(EDGED_COPY, (x-8, y-8),
-                                            (x+w+8, y+h+8), (0, 0, 255), 2)
-                                # Draw the bounding box on the threshold frame
+                                              (x+w+8, y+h+8), (0, 0, 255), 2)
+                                # Draw the bounding box on the threshold frame.
                                 cv2.rectangle(THRESHOLD_COPY, (x-8, y-8),
-                                            (x+w+8, y+h+8), (0, 0, 255), 2)
+                                              (x+w+8, y+h+8), (0, 0, 255), 2)
 
-                                # Check for a finish line crossing
+                                # Check for a finish line crossing.
                                 if (FL_X1 <= cX <= FL_X2) and (FL_Y1 <= cY <= FL_Y2):
-                                    Finish_colour = (0, 255, 0)
+                                    FINISH_LINE_COLOUR = (0, 255, 0)
                                     if not FINISHED:
                                         finished()
                                         FINISHED = True
                                 else:
-                                    Finish_colour = (0, 0, 255)
+                                    FINISH_LINE_COLOUR = (0, 0, 255)
                                     FINISHED = False
 
-                # elif len(approx) == 4:
-                #     # squares and rectangles.
+                elif len(approx) == 4:
+                    # squares and rectangles.
+                    pass
+
                 #     M = cv2.moments(c)
                 #     if M['m00'] != 0:
                 #         cX = int(M['m10'] / M['m00'])
@@ -324,8 +390,8 @@ if __name__ == "__main__":
                 #             ar = w / float(h)
                 #             cv2.drawContours(FRAME, [c], -1, (0, 0, 255), 3)
                 #             if 0.85 <= ar <= 1.15:
-                #                 cv2.putText(FRAME, 'Square', (cX-24, cY-16), cv2.FONT_HERSHEY_SIMPLEX,
-                #                             1, TXT_COLOUR, 2)
+                #                 cv2.putText(FRAME, 'Square', (cX-24, cY-16),
+                #                             cv2.FONT_HERSHEY_SIMPLEX, 1, TXT_COLOUR, 2)
                 #             else:
                 #                 cv2.putText(FRAME, 'Rectangle', (cX - 24, cY - 16),
                 #                             cv2.FONT_HERSHEY_SIMPLEX, 1, TXT_COLOUR, 2)
@@ -334,14 +400,14 @@ if __name__ == "__main__":
                 #                         cv2.FONT_HERSHEY_SIMPLEX, 1, TXT_COLOUR, 2)
 
                 else:
-                    # Do something
+                    # Do something for other shapes.
                     pass
 
-        cv2.rectangle(FRAME, finish_line_1, finish_line_2, Finish_colour, 3)
+        cv2.rectangle(FRAME, FINISH_LINE_1, FINISH_LINE_2, FINISH_LINE_COLOUR, 3)
         cv2.putText(FRAME, 'FINISH', (FL_X1 - 32, FL_Y2 + 32),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, Finish_colour, 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, FINISH_LINE_COLOUR, 2)
 
-        # Draw the parametres on the screen
+        # Draw the parametres on the screen.
         cv2.rectangle(FRAME, (0, 0), (300, 100), (255, 255, 255), -1)
         cv2.putText(FRAME, 'parameter 1:', (2, 24),
                     cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 0, 0), 2)
@@ -350,21 +416,23 @@ if __name__ == "__main__":
         cv2.putText(FRAME, 'parameter 3:', (2, 72),
                     cv2.FONT_HERSHEY_SIMPLEX, .75, (0, 0, 0), 2)
 
-        # Put the title on canny edge detection
+        # Put the title on canny edge detection.
         cv2.rectangle(EDGED_COPY, (0, 0), (450, 50), (255, 255, 255), -1)
         cv2.putText(EDGED_COPY, 'Canny edge detection', (4, 32),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.25, (0, 0, 0), 2)
 
-        # Put the title on threshold
+        # Put the title on threshold.
         cv2.rectangle(THRESHOLD_COPY, (0, 0), (350, 50), (255, 255, 255), -1)
         cv2.putText(THRESHOLD_COPY, 'Binary threshold', (4, 32),
                     cv2.FONT_HERSHEY_SIMPLEX, 1.25, (0, 0, 0), 2)
 
+        # Show the resulting frames.
         cv2.imshow('frame', FRAME)
-        cv2.imshow('threshold', THRESHOLD)
-        cv2.imshow('edged', EDGED)
+        cv2.imshow('threshold', THRESHOLD_COPY)
+        cv2.imshow('edged', EDGED_COPY)
 
         if cv2.waitKey(1) & 0xff == ord('q'):
+            # If the "q" is pressed, close to program by breaking the loop.
             break
 
         if SAVE_FRAMES:
@@ -375,16 +443,18 @@ if __name__ == "__main__":
                 NOT_SAVED = True
 
             if NOT_SAVED:
-                # Save the input
+                # Save the input.
                 cv2.imwrite('input.png', INPUT_FRAME)
-                # Save the result
+                # Save the result.
                 cv2.imwrite('result.png', FRAME)
-                # Save the threshold image
+                # Save the threshold image.
                 cv2.imwrite('threshold.png', THRESHOLD_COPY)
-                # Save the canny edge detection result
+                # Save the canny edge detection result.
                 cv2.imwrite('canny.png', EDGED_COPY)
             print('saved frames')
             NOT_SAVED = False
 
+    # Release the capture object.
     CAP.release()
+    # Destroy all windows and finish the program.
     cv2.destroyAllWindows()
