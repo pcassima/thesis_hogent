ade389ea95b3a98924d6102f6b7994a27c8b9207
Pieter-Jan Cassiman <pieterjan.cassiman@gmail.com>
2019-05-18 11:48:05 +0200

    Update nav.js
    Updated function to animate hamburger menu (goes from hamburger to cross wen the menu is open)

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -1,8 +1,19 @@
-function myFunction() {
-    let element = document.getElementById("nav-list");
-    if (element.classList.contains("active")) {
-        element.classList.remove("active");
+function hamburgerNav() {
+    // Get the navigation list from the page
+    let nav_list = document.getElementById('nav-list');
+    // Get the hamburger from the page (to change the shape)
+    let hamburger = document.getElementById('hamburger');
+
+    // Add or remove the active class from the navigation list
+    if (nav_list.classList.contains('active')) {
+        nav_list.classList.remove('active');
     } else {
-        element.classList.add("active");
+        nav_list.classList.add('active');
+    }
+    // Add or remove the active class from the hamburger
+    if (hamburger.classList.contains('active')) {
+        hamburger.classList.remove('active');
+    } else {
+        hamburger.classList.add('active');
     }
 }
